3aadf00ed04a229edc30d07f0db64a9b8c175381
cedric <cedric.pk22@gmail.com>
2022-10-04 11:27:37 -0400

    Add hardhat config & scripts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,47 @@
-require("@nomicfoundation/hardhat-toolbox");
+// require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-ethers');
 
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+const MNEMONIC = process.env.MNEMONIC;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  defaultNetwork: "bsc",
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    bsc: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      gasPrice: 20000000000,
+      accounts: {mnemonic: MNEMONIC}
+    },
+    bsctestnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: {mnemonic: MNEMONIC}
+    },
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: {mnemonic: MNEMONIC}
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 40000
+  }
 };
